9b6df4c7549e1d3cab56b2e525e3ad1a7e2fdd49
Bohdan Vlasenko <bodyavl228@gmail.com>
2023-09-04 16:10:11 +0300

    removed unnecessary routes

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -11,12 +11,6 @@ import { Movie } from './entities';
 @Controller('movie')
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
-
-  @Post()
-  create(@Body() createMovieDto: CreateMovieDto) {
-    return this.movieService.create(createMovieDto);
-  }
-
   
   @Get()
   async findAll() {
@@ -27,15 +21,5 @@ export class MovieController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.movieService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
-    return this.movieService.update(+id, updateMovieDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.movieService.remove(+id);
   }
 }
