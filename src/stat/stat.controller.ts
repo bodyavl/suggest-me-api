2006ff727cc0617428abb17f4cf382d6b7335574
Bohdan Vlasenko <bodyavl228@gmail.com>
2023-09-06 16:36:34 +0300

    stat removed unnecessary routes

--- src/stat/stat.controller.ts
+++ src/stat/stat.controller.ts
@@ -6,10 +6,4 @@ import { AccessTokenGuard } from '../auth/guard';
 @Controller('stat')
 export class StatController {
   constructor(private readonly statService: StatService) {}
-
-  @UseGuards(AccessTokenGuard)
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.statService.findOne(+id);
-  }
 }
